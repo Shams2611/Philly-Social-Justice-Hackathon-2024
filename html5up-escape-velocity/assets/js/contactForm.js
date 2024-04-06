3fce4aa4f092730c16caeb6ee019f27df2a95355
nwumefr <chibuikenwume@gmail.com>
2024-04-06 13:25:07 -0400

    editing contact.html

---
+++ html5up-escape-velocity/assets/js/contactForm.js
@@ -0,0 +1,17 @@
+function getInfo(){
+    info = {
+        fname: document.getElementById("Name").value,
+        number: document.getElementById("number").value,
+        canText: document.getElementById("text").checked,
+        time: document.getElementById("time").value
+    };
+    console.log(info);
+
+    if(info.canText == true) {
+        info.canText = "Yes";
+    }
+    else {
+        info.canText = "No";
+    }
+    window.open(`mailto:maxkchiu@gmail.com?subject=Minors Seeking Judicial Bypass&body=First Name: ${info.fname}%0D%0ALast Name: ${info.lname}%0D%0APhone Number: ${info.number}%0D%0ACan we text you: ${info.canText}%0D%0APreferred Time of Contact: ${info.time}`);
+}
