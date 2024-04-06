d0d80b4726ca81295976e225f41548c18a2f553c
Max Chiu <mc4487@drexel.edu>
2024-04-06 13:15:08 -0400

    contact form opens email

--- contact_form_demo/script.js
+++ contact_form_demo/script.js
@@ -7,6 +7,12 @@ function getInfo(){
         time: document.getElementById("time").value
     };
     console.log(info);
-    mail = ''
-    window.open(`mailto:maxkchiu@gmail.com?subject=subject&body=First Name: ${info.fname}%0D%0ALast Name: ${info.lname}%0D%0APhone Number: ${info.number}%0D%0ACan we text you: ${info.canText}%0D%0APreferred Time of Contact: ${info.time}`);
+
+    if(info.canText == true) {
+        info.canText = "Yes";
+    }
+    else {
+        info.canText = "No";
+    }
+    window.open(`mailto:maxkchiu@gmail.com?subject=Minors Seeking Judicial Bypass&body=First Name: ${info.fname}%0D%0ALast Name: ${info.lname}%0D%0APhone Number: ${info.number}%0D%0ACan we text you: ${info.canText}%0D%0APreferred Time of Contact: ${info.time}`);
 }
