18a2c2ae326b1c67d961441443763d01b5cacb50
elan_rubin <85769784+Elan-Rubin@users.noreply.github.com>
2024-04-06 17:13:19 -0400

    push for carl code cleanup

--- mapping/script.js
+++ mapping/script.js
@@ -1,5 +1,36 @@
+/*
+777 Appletree St 7th floor, Philadelphia, PA 19106
+39.953560 -75.151930
+https://maps.app.goo.gl/X2KCULncJkLm9MReA
+
+2751 Comly Rd, Philadelphia, PA 19154 
+40.103990 -75.000660
+https://maps.app.goo.gl/5pd2pSFdiLwv94Uv7
+
+1144 Locust St, Philadelphia, PA 19107
+39.947472 -75.160683
+https://maps.app.goo.gl/Ko5B7xx3msH4yQmf8
+
+317 Broadway, Camden, NJ 08103
+39.941010 -75.119560
+https://maps.app.goo.gl/uTQkX8gZ2PJ4g5Qy7
+
+502 Kings Hwy N, Cherry Hill, NJ 08034
+39.926830 -74.996742
+https://maps.app.goo.gl/oPCjt3AQcD39dc5CA
+
+1501 Arch St, Philadelphia, PA 19102
+39.95512993309987, -75.1652010117053
+https://maps.app.goo.gl/32rnpqoDT8SvK6f48
+
+1441 Sansom St, Philadelphia, PA 19102
+39.95034245750883, -75.16560438125407
+https://maps.app.goo.gl/GBvmrWURNe44vKTM9
+*/
+
 const clinics = [
     {
+        "type": "clinic",
         "name": "Philadelphia Women's Center",
         "links": "https://www.thewomenscenters.com/philadelphiawomenscenter/",
         "addr": "777 Appletree St 7th floor, Philadelphia, PA 19106",
@@ -9,13 +40,16 @@ const clinics = [
         "pillInfo": "offered b4 9 months",
         "surgeryOffered": false,
         "surgeryInfo": "lorem ipsum",
-        "long": 39.9596,
-        "lat": -75.1904
+         
+        "long": 39.953560,
+        "lat": -75.151930,
+        "hours": "hours : hours"
     }
 ];
 
 const attorneys = [
     {
+        "type": "attorney",
         "name": "Defenders Association of Philadelphia",
         "links": "https://www.thewomenscenters.com/philadelphiawomenscenter/",
         "addr": "1441 Sansom St, Philadelphia, PA 19102",
@@ -24,18 +58,22 @@ const attorneys = [
         "email": "JudicialBypass@philadefender.org",
         "otherInfo": "",
         "long": 39.9596,
-        "lat": -75.1924
+        "lat": -75.1924,
+        "hours": "hours : hours"
     }
 ];
 
 const courthouses = [
     {
+        "type": "courthouse",
         "name": "Family Court",
         "links": "https://www.thewomenscenters.com/philadelphiawomenscenter/",
         "addr": "1501 Arch St, Philadelphia, PA 19102",
         "otherInfo": "This is where the judicial bypass hearing will take place in-person",
         "long": 39.9596,
-        "lat": -75.1914
+        "lat": -75.1914,
+        "hours": "hours : hours",
+        "contacts":"contact goes here"
     }
 ];
 
@@ -113,12 +151,28 @@ console.log(markersArr)
 //     });
 // }
 
+
+//this is janky to have the code pasted this many times lol
+//should be fixed later
 clinics.forEach((clinic) => {
     let marker = L.marker([clinic.long, clinic.lat], markerOptionsArr[0]);
     marker.bindPopup(clinic.name).openPopup();
     marker.addTo(map).on('click', function(e) {
         document.getElementById("name-label").innerText = clinic.name;
+        document.getElementById("addr-label").innerText = clinic.addr;
+        document.getElementById("hours-label").innerText = clinic.hours;
+
         document.getElementById("methods-info").style.display = "block";
+
+        document.getElementById("contacts-label").innerHTML = clinic.contacts
+
+        document.getElementById("pill-check").style.display = clinic.pillOffered ? "block" : "none"
+        document.getElementById("pill-x").style.display = !clinic.pillOffered ? "block" : "none"
+        document.getElementById("pill-info").innerHTML = clinic.pillInfo
+
+        document.getElementById("surgical-check").style.display = clinic.surgeryOffered ? "block" : "none"
+        document.getElementById("surgical-x").style.display = !clinic.surgeryOffered ? "block" : "none"
+        document.getElementById("surgical-info").innerHTML = clinic.surgeryInfo
     });
 });
 
@@ -127,6 +181,11 @@ attorneys.forEach((attorney) => {
     marker.bindPopup(attorney.name).openPopup();
     marker.addTo(map).on('click', function(e) {
         document.getElementById("name-label").innerText = attorney.name;
+        document.getElementById("addr-label").innerText = attorney.addr;
+        document.getElementById("hours-label").innerText = attorney.hours;
+
+        document.getElementById("contacts-label").innerHTML = attorney.contacts
+
         document.getElementById("methods-info").style.display = "none";
     });
 });
@@ -136,6 +195,11 @@ courthouses.forEach((courthouse) => {
     marker.bindPopup(courthouse.name).openPopup();
     marker.addTo(map).on('click', function(e) {
         document.getElementById("name-label").innerText = courthouse.name;
+        document.getElementById("addr-label").innerText = courthouse.addr;
+        document.getElementById("hours-label").innerText = courthouse.hours;
+
+        document.getElementById("contacts-label").innerHTML = courthouse.contacts
+
         document.getElementById("methods-info").style.display = "none";
     });
 });
