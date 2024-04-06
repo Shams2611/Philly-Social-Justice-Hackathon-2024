0c8fa4bc6f5c911aec8e765ee657f27014fab4f9
Max Chiu <mc4487@drexel.edu>
2024-04-06 12:41:54 -0400

    opens mailing window with subject and body

--- contact_form_demo/script.js
+++ contact_form_demo/script.js
@@ -3,8 +3,10 @@ function getInfo(){
         fname: document.getElementById("fname").value,
         lname: document.getElementById("lname").value,
         number: document.getElementById("number").value,
-        canText: document.getElementById("text").value,
+        canText: document.getElementById("text").checked,
         time: document.getElementById("time").value
     };
     console.log(info);
+    mail = ''
+    window.open(`mailto:maxkchiu@gmail.com?subject=subject&body=First Name: ${info.fname}%0D%0ALast Name: ${info.lname}%0D%0APhone Number: ${info.number}%0D%0ACan we text you: ${info.canText}%0D%0APreferred Time of Contact: ${info.time}`);
 }
